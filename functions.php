bde5fb250e62cfa8c3267922379659034c95bc85
Ryan <34662336+Razza789@users.noreply.github.com>
2023-12-18 11:18:01 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function getConnection() {
-    try {$connection = new PDO("mysql:host=localhost;dbname=unn_w20017978","unn_w20017978", "RyanFieldWeb1!");
+    try {$connection = new PDO("mysql:host=localhost;dbname=","", "");
     $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     return $connection;
     } catch (Exception $e) {
